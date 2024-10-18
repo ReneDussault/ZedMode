24939e80e5fa6d4b9644d3a7d6372c8eddf95933
Rene Dussault <63034012+ReneDussault@users.noreply.github.com>
2024-10-18 02:04:56 -0400

    Add files via upload
    updates

--- background.js
+++ background.js
@@ -1,3 +1,21 @@
+// Function to update the icon based on the state (enabled or disabled)
+function updateIcon(darkModeEnabled) {
+    const iconPath = darkModeEnabled ? "images/icon" : "images/inactive-icon";
+    browser.action.setIcon({
+        path: {
+            "16": iconPath + "16.png",
+            "48": iconPath + "48.png",
+            "128": iconPath + "128.png"
+        }
+    });
+}
+
+// On extension load, get the saved state from storage and update the icon
+browser.storage.local.get("darkModeEnabled").then((result) => {
+    const darkModeEnabled = result.darkModeEnabled || false; // Default to false if undefined
+    updateIcon(darkModeEnabled);  // Set the icon based on the state
+});
+
 // When the extension icon is clicked, toggle dark mode
 browser.action.onClicked.addListener((tab) => {
     // Get the current state from storage
@@ -6,6 +24,9 @@ browser.action.onClicked.addListener((tab) => {
 
         // Save the new state in storage
         browser.storage.local.set({ darkModeEnabled });
+
+        // Update the icon based on the new state
+        updateIcon(darkModeEnabled);
 
         // Send a message to the content script to enable/disable dark mode
         browser.tabs.sendMessage(tab.id, { action: "toggleDarkMode", enabled: darkModeEnabled });
